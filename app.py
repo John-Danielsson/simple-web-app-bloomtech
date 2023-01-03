34c9364da0517d672b1358accc725c07a2d69d2e
John Danielsson <JohnDanielsson@JDs-MacBook-Pro-work.local>
2023-01-02 22:59:05 -0800

    added sql libraries

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import tweepy
+from flask_sqlalchemy import SQLAlchemy
+server = Flask(__name__)
+db = SQLAlchemy(server)
+
+# Instiantiate Flask application
+app = Flask(__name__)
+
+
+@app.route('/')
+def landingPage():
+    return '<p>This is the landing page.</p>'
+
+# Terminal command to run:
+# FLASK_APP=app.py flask run
+# flask --app app run
+
+# lang = en
+# created_at
