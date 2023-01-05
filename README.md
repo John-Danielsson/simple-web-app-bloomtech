d6104d1c07917eebc58a7d4f448bf2c8991a874c
John Danielsson <JohnDanielsson@JDs-MacBook-Pro-work.local>
2023-01-05 14:29:01 -0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # web-app-project-bloomtech
-This is a web app showing the sentiment of Tweets in the last 24 hours for a subject given by the user.
+This is a web app showing the sentiment of Tweets in the last 48 hours for a subject given by the user.
--- app.py
+++ app.py
@@ -1,20 +1,53 @@
-from flask import Flask
-import tweepy
-from flask_sqlalchemy import SQLAlchemy
-server = Flask(__name__)
-db = SQLAlchemy(server)
+# from flask import Flask
+# import tweepy
+# from flask_sqlalchemy import SQLAlchemy
 
-# Instiantiate Flask application
-app = Flask(__name__)
+# # Instantiate SQL database
+# server = Flask(__name__)
+# db = SQLAlchemy(server)
+
+# # Instiantiate Flask application
+# app = Flask(__name__)
 
 
-@app.route('/')
-def landingPage():
-    return '<p>This is the landing page.</p>'
+# @app.route('/')
+# def landingPage():
+#     return '<p>This is the landing page.</p>'
 
-# Terminal command to run:
-# FLASK_APP=app.py flask run
-# flask --app app run
+# # Terminal command to run:
+# # FLASK_APP=app.py flask run
+# # flask --app app run
 
-# lang = en
-# created_at
+# # lang = en
+# # created_at
+import math
+
+
+def isPrime(n: int) -> bool:
+    if n > 1:
+        for i in range(2, int(math.sqrt(n))+1):
+            if n % i == 0:
+                return False
+        return True
+    return False
+
+
+def bigList() -> list:
+    result = []
+    for i in range(2, 1000001):
+        if isPrime(i):
+            result.append(i)
+    return result
+
+
+def bigListStr(arr: list) -> str:
+    if len(arr) <= 15:
+        return str(arr)
+    result = '['
+    for i in range(0, len(arr), 15):
+        result += f'{str(arr[i:i+15])[1:-1]},\n'
+    return result + ']'
+
+
+with open('list2.txt', 'w') as f:
+    f.write(str(bigListStr(bigList())))
